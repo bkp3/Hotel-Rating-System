85c2594bea396fb978d9fe8a830b2bf2ef094153
Bhola Kumar <bholakumar.7372@gmail.com>
2023-02-12 01:26:59 +0530

    Service Discovery Client
    Service Discovery Client

--- UserService/src/main/java/com/user/entities/User.java
+++ UserService/src/main/java/com/user/entities/User.java
@@ -3,11 +3,7 @@ package com.user.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
