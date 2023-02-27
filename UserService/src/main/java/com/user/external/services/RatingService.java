baeb3ebbaa4319c5a59b4fa0a4fb9d05a42a6d8a
Bhola Kumar <bholakumar.7372@gmail.com>
2023-02-28 00:53:15 +0530

    API GATEWAY
    API GATEWAY

--- UserService/src/main/java/com/user/external/services/RatingService.java
+++ UserService/src/main/java/com/user/external/services/RatingService.java
@@ -1,6 +1,7 @@
 package com.user.external.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,15 +10,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 import com.user.entities.Rating;
 
-
 @FeignClient(name = "RATING-SERVICE")
 public interface RatingService {
 
 	@PostMapping("/ratings")
-	public Rating createRating(Rating rating);
+	public ResponseEntity<Rating> createRating(Rating rating);
 
 	@PutMapping("/ratings/{ratingId}")
-	public Rating updateRating(@PathVariable String ratingId, Rating rating);
+	public ResponseEntity<Rating> updateRating(@PathVariable String ratingId, Rating rating);
 
 	@DeleteMapping("/ratings/{ratingId}")
 	public void deleteRating(@PathVariable String ratingId);
